b52b71231d1083b028d971d544b9c6affc890acc
alvisyong <alvis20000929@yahoo.com.tw>
2019-04-03 23:20:00 +0800

    fptr

--- main.c
+++ main.c
@@ -7,17 +7,30 @@ int multiply(int a, int b) { return a * b; }
 int divided(int a, int b) { return a / b; }
 int main()
 {
-	int a, c;
+	int a, c,i;
 	char b;
-	printf("key a Function \nEX: 1 + 1\n");
+	char arr[]={'+','-','*','/'};
+        int (*fptr[])(int,int)={plus,minus,multiply,divided};
+        printf("key a Function \nEX: 1 + 1\n");
 	scanf("%i %c %i", &a, &b, &c);
+        for(i=0;i<4;i++)
+       {
+        if(b==arr[i])
+       {
+        
+        printf("%d%c%d=%d\n",a,b,c,fptr[i](a,c));
+       }
+      
+
+ }
 	//IF ELSE IF
-	if (b == '+')
+       
+/*	if (b == '+')
 		printf("%d %c %d = %d\n", a, b, c, plus(a, c));
 	else if (b == '-')
 		printf("%d %c %d = %d\n", a, b, c, minus(a, c));
 	else if (b == '*')
 		printf("%d %c %d = %d\n", a, b, c, multiply(a, c));
 	else if (b == '/')
-		printf("%d %c %d = %d\n", a, b, c, divided(a, c));
+		printf("%d %c %d = %d\n", a, b, c, divided(a, c));*/
 }
